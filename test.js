b3d247ae250b43793e7b901d766dd544d4f43206
Elliot Nelson <Elliot.Nelson@hbo.com>
2020-03-19 06:21:12 -0400

    Make title an optional parameter

--- test.js
+++ test.js
@@ -10,27 +10,29 @@ test('throws when required options are invalid', t => {
 		createUrl({compareTo: 23});
 	}, Error);
 	t.throws(() => {
-		createUrl({title: 23});
-	}, Error);
-	t.throws(() => {
 		createUrl({repo: 23});
 	}, Error);
 });
 
-test('should create the repo url', t => {
+test('should create repo url with only required params', t => {
 	const repo = 'abc/foo-repo';
+	const compareTo = 'rocket-branch';
+	t.is(createUrl({
+		repo,
+		compareTo
+	}), `https://github.com/${repo}/compare/master...${compareTo}?quick_pull=1`);
+});
+
+test('should create repo url with all possible params', t => {
+	const repo = 'abc/foo-repo';
+	const base = 'stable';
 	const compareTo = 'rocket-branch';
 	const title = 'awesome-title';
 	const assignee = 'hamxabaig';
 	const body = 'off-to-mars';
-	t.is(createUrl({
-		repo,
-		compareTo,
-		title
-	}), `https://github.com/${repo}/compare/master...${compareTo}?quick_pull=1&title=${title}`);
-	const url = createUrl({repo, compareTo, title, assignee, body});
+	const url = createUrl({repo, base, compareTo, title, assignee, body});
 	const {searchParams} = new URL(url);
-	t.true(url.startsWith(`https://github.com/${repo}/compare/master...${compareTo}`));
+	t.true(url.startsWith(`https://github.com/${repo}/compare/${base}...${compareTo}`));
 	t.is(searchParams.get('body'), body);
 	t.is(searchParams.get('assignee'), assignee);
 	t.is(searchParams.get('title'), title);
